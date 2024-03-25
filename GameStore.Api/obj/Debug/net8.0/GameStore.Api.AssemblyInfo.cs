6163f1b6ddea94e783c56bf0b641374bda796bd8
XeldarAlz <xeldaralz@hotmail.com>
2024-03-25 14:35:41 +0300

    Add new nougat package to apply parameter validation

--- GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
+++ GameStore.Api/obj/Debug/net8.0/GameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ee040cf6a2c1a529b9db31b0524794d4dd5964c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9b7ed0d41c2432b4f4441a3699b14a06219cd8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
